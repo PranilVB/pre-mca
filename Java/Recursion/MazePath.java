f5de68abadd12eab44db9eea028f3e12823fcda2
pranilvb <pranilvb@gmail.com>
2026-08-04 17:20:25 +0530

    Maze path

---
+++ Java/Recursion/MazePath.java
@@ -0,0 +1,21 @@
+/**
+ * MazePath
+ */
+public class MazePath {
+
+    public static void main(String[] args) {
+        Path(3, 3, "");
+    }
+    static void Path(int r,int c,String p){
+        if(r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            Path(r-1, c, p+'D');
+        }
+        if(c>1){
+            Path(r, c-1, p+'R');
+        }
+    }
+}
