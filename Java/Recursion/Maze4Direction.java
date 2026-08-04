9e0e942c232f7f72e234ba8feb35d5b059ddd822
pranilvb <pranilvb@gmail.com>
2026-08-04 23:27:47 +0530

    maze with 4D

---
+++ Java/Recursion/Maze4Direction.java
@@ -0,0 +1,42 @@
+/**
+ * Maze4Direction
+ */
+public class Maze4Direction {
+
+    public static void main(String[] args) {
+        boolean[][] board={
+            {true,true,true},
+            {true,true,true},
+            {true,true,true}
+        } ;
+        Dir("",board,0,0);
+    }
+    static void Dir(String p,boolean[][] maze,int r,int c){
+        if(r==maze.length-1&&c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+        if(!maze[r][c]){
+            return;
+        }
+maze[r][c] = false;
+
+if (r < maze.length - 1) {
+    Dir(p + "-D-", maze, r + 1, c);
+}
+
+if (c < maze[0].length - 1) {
+    Dir(p + "-R-", maze, r, c + 1);
+}
+
+if (r > 0) {
+    Dir(p + "-U-", maze, r - 1, c);
+}
+
+if (c > 0) {
+    Dir(p + "-L-", maze, r, c - 1);
+}
+
+maze[r][c] = true;
+    }
+}
