98e751517d179ace67ea4224bf4d2184bb4fd979
pranilvb <pranilvb@gmail.com>
2026-08-08 20:29:25 +0530

    OOP

---
+++ Java/OOP/ClassExample.java
@@ -0,0 +1,12 @@
+public class ClassExample {
+
+    static class Student {
+        int rollNumber;
+        String name;
+        float marks;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Student class created.");
+    }
+}
